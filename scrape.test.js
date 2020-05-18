77edf43553b0655473ab616e22cdf4c44e72bea7
valmassoi <valmassoi@pm.me>
2020-05-18 11:05:14 -0400

    cleanup

--- scrape.test.js
+++ scrape.test.js
@@ -2,17 +2,6 @@ var fs = require("fs");
 
 const { getRougeStock } = require("./scrape");
 
-
-// let mockHtml;
-// beforeAll(async (done) => {
-//   await fs.readFile(__dirname + "/mock.html", (err, data) => {
-//         if (err) {
-//         throw err;
-//         }
-//         mockHtml = data.toString();
-//     });
-//     return done()
-// });
 describe("getRougeStock", () => {
     const mockHtml = fs.readFileSync(__dirname + "/mock.html");
     it("finds stock", () => {
